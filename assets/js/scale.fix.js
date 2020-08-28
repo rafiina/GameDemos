eaf36b664087cc542a2480072774dd0898ef0d31
rafiina <14363298+rafiina@users.noreply.github.com>
2020-08-27 23:20:50 -0500

    Theme setup

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -1,22 +1,20 @@
-fixScale = function (doc) {
-  var addEvent = "addEventListener",
-    type = "gesturestart",
-    qsa = "querySelectorAll",
-    scales = [1, 1],
-    meta = qsa in doc ? doc[qsa]("meta[name=viewport]") : [];
+fixScale = function(doc) {
 
-  function fix() {
-    meta.content =
-      "width=device-width,minimum-scale=" +
-      scales[0] +
-      ",maximum-scale=" +
-      scales[1];
-    doc.removeEventListener(type, fix, true);
-  }
+	var addEvent = 'addEventListener',
+	    type = 'gesturestart',
+	    qsa = 'querySelectorAll',
+	    scales = [1, 1],
+	    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
 
-  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-    fix();
-    scales = [0.25, 1.6];
-    doc[addEvent](type, fix, true);
-  }
+	function fix() {
+		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+		doc.removeEventListener(type, fix, true);
+	}
+
+	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+		fix();
+		scales = [.25, 1.6];
+		doc[addEvent](type, fix, true);
+	}
+
 };
